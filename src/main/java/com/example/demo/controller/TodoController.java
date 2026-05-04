f2d833190f093e26644617ee47ed381ff10eaac7
raksabackend <kpeou@paragoniu.edu.kh>
2026-05-04 15:22:39 +0700

    Add delete todo by id

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.Todo;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -25,8 +24,23 @@ public class TodoController {
     }
 
     @GetMapping("/{id}")
-    public Todo getTodoById(@PathVariable("id") Integer id) {
+    public Todo getTodoById(@PathVariable("id") Long id) {
         return todos.stream().filter(c -> Objects.equals(c.getId(), id)).map(m -> new Todo(m.getId(), m.getTitle(), m.isCompleted())).findFirst().orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public Todo deleteById(@PathVariable("id") Long id) {
+
+        Todo todoToDelete = todos.stream()
+                .filter(todo -> id.equals(todo.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if (todoToDelete!=null) {
+            todos.remove(todoToDelete);
+        }
+
+        return todoToDelete;
     }
 
 }

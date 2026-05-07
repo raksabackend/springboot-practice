448c318e7b2fdb47f5cb9919e643031e2658910d
raksabackend <kpeou@paragoniu.edu.kh>
2026-05-07 13:24:02 +0700

    Added Response Entity

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.Todo;
 import com.example.demo.dto.UpdateDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,24 +18,25 @@ public class TodoController {
     private List<Todo> todos = new ArrayList<>();
 
     @PostMapping
-    public Todo createTodo(@RequestBody Todo todo) {
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
         todos.add(todo);
-        return todo;
+        return ResponseEntity.ok().body(todo);
     }
 
     @GetMapping
-    public List<Todo> getTodos() {
-        return todos;
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok().body(todos);
     }
 
 
     @GetMapping("/{id}")
-    public Todo getTodoById(@PathVariable("id") Long id) {
-        return todos.stream().filter(c -> Objects.equals(c.getId(), id)).map(m -> new Todo(m.getId(), m.getTitle(), m.isCompleted())).findFirst().orElse(null);
+    public ResponseEntity<Todo> getTodoById(@PathVariable("id") Long id) {
+        Todo todo = todos.stream().filter(c -> Objects.equals(c.getId(), id)).map(m -> new Todo(m.getId(), m.getTitle(), m.isCompleted())).findFirst().orElse(null);
+        return ResponseEntity.ok().body(todo);
     }
 
     @DeleteMapping("/{id}")
-    public Todo deleteById(@PathVariable("id") Long id) {
+    public ResponseEntity<Todo> deleteById(@PathVariable("id") Long id) {
 
         Todo todoToDelete = todos.stream()
                 .filter(todo -> id.equals(todo.getId()))
@@ -45,11 +47,11 @@ public class TodoController {
             todos.remove(todoToDelete);
         }
 
-        return todoToDelete;
+        return ResponseEntity.ok().body(todoToDelete);
     }
 
     @PatchMapping("/{id}")
-    public Todo updateById(
+    public ResponseEntity<Todo> updateById(
             @PathVariable("id") Long id,
             @RequestBody UpdateDto updateTodo
     ) {
@@ -66,7 +68,7 @@ public class TodoController {
             todoToUpdate.setCompleted(updateTodo.isCompleted());
         }
 
-        return todoToUpdate;
+        return ResponseEntity.ok().body(todoToUpdate);
     }
 
 }
